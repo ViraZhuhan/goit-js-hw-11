a13beadc9c3c7a4e99f44d1964a3acd090d36b56
ViraZhuhan <virazhuhan@gmail.com>
2023-04-24 19:58:16 +0300

    add not async

---
+++ src/index.js
@@ -0,0 +1,106 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SeachApiService from './js/seach-service';
+import hitsTpl from './templates/hits.hbs';
+import LoadMoreBtn from './js/load-more-btn';
+import {
+  noSeachError,
+  onFetchError,
+  totalImagesFound,
+  reachedEndImages,
+} from './js/messages';
+
+const refs = {
+  seachForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const seachApiService = new SeachApiService();
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+refs.seachForm.addEventListener('submit', onSeach);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+function onSeach(e) {
+  e.preventDefault();
+
+  seachApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+
+  seachApiService.resetPage();
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  onFetchHits();
+}
+
+function onLoadMore() {
+  loadMoreBtn.disable();
+  if (
+    seachApiService.page * seachApiService.perPage >
+    seachApiService.totalHits
+  ) {
+    reachedEndImages();
+    loadMoreBtn.hide();
+  }
+  onFetchHits();
+}
+
+function onFetchHits() {
+  
+  seachApiService
+    .fetchHeats()
+    .then(hits => {
+      createGallery(hits);
+      simpleLightbox.refresh();
+      loadMoreBtn.enable();
+    })
+    .catch(error => {
+      onFetchError();
+      clearGallery();
+    });
+}
+
+// async function onFetchHits() {
+//   try {
+//     const hits = await seachApiService.fetchHits()
+//     createGallery(hits);
+//     simpleLightbox.refresh();
+//     loadMoreBtn.enable();
+//   } catch (error) {
+//      onFetchError();
+//         clearGallery();
+//   }
+//   }
+
+
+function createGallery(hits) {
+  if (seachApiService.query === '') {
+    loadMoreBtn.hide();
+    onFetchError();
+    return;
+  }
+
+  if (seachApiService.totalHits === 0) {
+    loadMoreBtn.hide();
+    noSeachError();
+    return;
+  } 
+  
+  else {
+    clearGallery();
+    loadMoreBtn.enable();
+    totalImagesFound(seachApiService.totalHits);
+    return refs.gallery.insertAdjacentHTML('beforeend', hitsTpl(hits));
+  }
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
---
+++ src/js/seach-service.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+const API_KEY = `35492666-a0d5fecb0778e5a5f1e0518fb`;
+const URL = `https://pixabay.com/api/`;
+
+export default class SeachApiService {
+  constructor() {
+    this.page = 1;
+    this.perPage = 40;
+    this.imageType = 'photo';
+    this.orientation = 'horizontal';
+    this.safesearch = true;
+    this.searchQuery = '';
+    this.totalHits = 0;
+  }
+
+  // async fetchHits() {
+  //   const {data} = await axios.get(
+  //     `${URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+  //   );
+
+  //   this.totalHits = data.totalHits;
+  //   this.incrementPage();
+
+  //   console.log(this.searchQuery)
+  //   return data;
+  // }
+
+  // async fetchHeats() {
+  //   const url = `${URL}/?key=${API_KEY}&q=${this.searchQuery}&language=en&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+
+  //   const response = await axios.get(url);
+  //   return response.data;
+  // }
+
+
+
+   fetchHeats() {
+    return fetch(
+        `${URL}?key=${API_KEY}&q=${this.seachQuery}&page=${this.page}&per_page=${this.perPage}&image_type=${this.imageType}&orientation=${this.orientation}&safesearch=${this.safesearch}`
+      )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.incrementPage();
+        this.totalHits = data.totalHits;
+        return data.hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.seachQuery;
+  }
+
+  set query(newQuery) {
+    this.seachQuery = newQuery;
+  }
+}
